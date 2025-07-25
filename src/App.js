22fc2c95fe7036f081fee940c59a413f659bb151
AidnacNelson <candiapnelson@gmail.com>
2025-07-24 23:47:46 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-useEffect(() => {
-  console.log("🔑 REACT_APP_AIRTABLE_API_KEY →", process.env.REACT_APP_AIRTABLE_API_KEY);
-}, []);
-
 function App() {
   return (
     <div className="App">
