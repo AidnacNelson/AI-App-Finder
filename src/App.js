8bc97bd1ca8a39812fcbd5058e8cf52c9501aed6
AidnacNelson <candiapnelson@gmail.com>
2025-07-25 13:27:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import SunflowerStack from './components/SunflowerStack';
-import './App.css';
+// src/App.js
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return <SunflowerStack />;
+// Your “main” UI:
+import SunflowerStack from "./components/SunflowerStack";
+
+// Detail pages you created:
+import AppDetail      from "./pages/AppDetail";
+import PersonaDetail  from "./pages/PersonaDetail";
+
+export default function App() {
+  return (
+    <Routes>
+      {/* “/” now loads your SunflowerStack */}
+      <Route path="/" element={<SunflowerStack />} />
+
+      {/* Detail views */}
+      <Route path="/app/:appId"       element={<AppDetail />} />
+      <Route path="/persona/:personaId" element={<PersonaDetail />} />
+    </Routes>
+  );
 }
-
-export default App;
